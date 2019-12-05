0f8906dd2119dd672a21e176e438d3c9b8a32835
Zhao Guangjian <zgj0315@gmail.com>
2019-12-05 16:24:52 +0800

    build a java project for test

---
+++ src/main/java/org/after90/Application.java
@@ -0,0 +1,19 @@
+
+package org.after90;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhaogj
+ */
+@SpringBootApplication
+@Slf4j
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class);
+  }
+
+}
