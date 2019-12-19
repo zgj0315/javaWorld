1eeaacd494cfc020ac887436d8768c3baf9a7b2b
Zhao Guangjian <zgj0315@gmail.com>
2019-12-19 16:43:14 +0800

    springboot version change to v2.1.6

--- src/test/java/org/after90/sentinel/HelloWorldTest.java
+++ src/test/java/org/after90/sentinel/HelloWorldTest.java
@@ -2,7 +2,8 @@ package org.after90.sentinel;
 
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
